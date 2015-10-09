b412d1337949960dd11df70938e6c34cf84d6633
Robert Farmer <rjfarmer@asu.edu>
2015-10-09 10:21:28 -0700

    Move setting the mixing colors to init so there use editable.

--- mesaPlot.py
+++ mesaPlot.py
@@ -325,6 +325,18 @@ class plot(object):
 					'clr_LightOliveGreen':[  0.6, 0.8, 0.6],
 					'clr_CadetBlue':[  0.372, 0.62, 0.628],
 					'clr_Beige':[  0.96, 0.96, 0.864]}
+		
+		self.mix_names=['None','Conv','Soften','Over','Semi','Thermo','Rot','Mini','Anon']
+		self.mix_col=[self.colors['clr_SeaGreen'], #None
+					  self.colors['clr_LightSkyBlue'], #Convection
+					  self.colors['clr_LightSteelBlue'], #Softened convection
+					  self.colors['clr_SlateGray'], # Overshoot
+					  self.colors['clr_Lilac'], #Semi convection
+					  self.colors['clr_Beige'], #Thermohaline
+					  self.colors['clr_BrightBlue'], #Rotation
+					  self.colors['clr_Coral'], #Minimum
+					  self.colors['clr_Tan'] #Anonymous
+					  ]
 					
 	
 	def _getMESAPath(self):
@@ -446,24 +458,8 @@ class plot(object):
 		return mixList
 
 
-	def _setMixRegionsCol(self,kip=False,mix=False):
-		#cmap = mpl.colors.ListedColormap([[0.18, 0.545, 0.34], [0.53, 0.808, 0.98],
-			#[0.96, 0.96, 0.864], [0.44, 0.5, 0.565],[0.8, 0.6, 1.0],
-			#[0.0, 0.4, 1.0],[1.0, 0.498, 0.312],[0.824, 0.705, 0.55]])      
-		self.mix_names=['None','Conv','Soften','Over','Semi','Thermo','Rot','Mini','Anon']
-		self.mix_col=[self.colors['clr_SeaGreen'], #None
-					  self.colors['clr_LightSkyBlue'], #Convection
-					  self.colors['clr_LightSteelBlue'], #Softened convection
-					  self.colors['clr_SlateGray'], # Overshoot
-					  self.colors['clr_Lilac'], #Semi convection
-					  self.colors['clr_Beige'], #Thermohaline
-					  self.colors['clr_BrightBlue'], #Rotation
-					  self.colors['clr_Coral'], #Minimum
-					  self.colors['clr_Tan'] #Anonymous
-					  ]
-		
+	def _setMixRegionsCol(self,kip=False,mix=False):		
 		cmap = mpl.colors.ListedColormap(self.mix_col)
-	
 	
 		cmap.set_over((1., 1., 1.))
 		cmap.set_under((0., 0., 0.))
