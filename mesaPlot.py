2c1c088dded49006043ef608f22738ddc96f02e2
Robert Farmer <rjfarmer@asu.edu>
2016-06-06 14:09:37 -0700

    Fix MESA_DIR

--- mesaPlot.py
+++ mesaPlot.py
@@ -386,14 +386,17 @@ class plot(object):
 		self.elementsPretty=['n','H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Uub', 'Uut', 'Uuq', 'Uup', 'Uuh', 'Uus', 'Uuo',]
 		self.elements=[x.lower() for x in self.elementsPretty]
 					
+		self._getMESAPath()
 	
 	def _getMESAPath(self):
 		self.mesa_dir=os.getenv("MESA_DIR")
-		if self.mesa_dir==None:
-			raise ValueError("Must set $MESA_DIR in terminal")
+		#if self.mesa_dir==None:
+			#raise ValueError("Must set $MESA_DIR in terminal or call setMESAPath(mesa_dir)")
+		
+	def setMESAPath(self,mesa_dir):
+		self.mesa_dir=mesa_dir
 			
 	def _loadBurnData(self):
-		self._getMESAPath()
 		dataDir=self.mesa_dir+"/data/star_data/plot_info/"
 		
 		self._hburn=np.genfromtxt(dataDir+"hydrogen_burn.data",names=["logRho","logT"])
