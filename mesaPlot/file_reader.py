18aaf191203bd99dc4da140013e3b9cd1cc7a052
Robert Farmer <r.j.farmer@uva.nl>
2018-03-06 10:47:52 +0100

    Fix emaill address

--- mesaPlot/file_reader.py
+++ mesaPlot/file_reader.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017, Robert Farmer rjfarmer@asu.edu
+# Copyright (c) 2017, Robert Farmer r.j.farmer@uva.nl
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
