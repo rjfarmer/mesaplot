d82c6bd99ecfbc7983aa22dc68508001fad694aa
Robert Farmer <r.j.farmer@uva.nl>
2018-06-29 12:19:09 +0200

    Fix utf-8 encoding issue with long description
    Reported By @pmartens53

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 import os
 from setuptools import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def read(fname):	
+    return open(os.path.join(os.path.dirname(__file__), fname),'r', encoding='utf-8').read()
 
 setup(name='mesaPlot',
       version='0.0',
