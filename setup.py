984c42c0f72a44971a7fde0bfb646c74305461e9
Robert Farmer <r.j.farmer@uva.nl>
2018-07-13 14:48:41 +0200

    Remove long_description from setup.py
    Too many issues with encoding
    
    Closes #15 #17

--- setup.py
+++ setup.py
@@ -3,14 +3,10 @@
 import os
 from setuptools import setup
 
-def read(fname):	
-    return open(os.path.join(os.path.dirname(__file__), fname),'r', encoding='utf-8').read()
-
 setup(name='mesaPlot',
       version='0.0',
       description='Library for reading and plotting MESA data',
       license="GPLv2+",
-      long_description=read('README.md'),
       author='Robert Farmer',
       author_email='r.j.farmer@uva.nl',
       url='https://github.com/rjfarmer/mesaPlot',
