c6d26a61d24a3791ef44ee5b054d182ab84adb2b
Robert Farmer <robert.j.farmer37@gmail.com>
2023-10-26 15:32:07 +0100

    Remove old comments

--- setup.py
+++ setup.py
@@ -3,28 +3,3 @@ from setuptools import setup
 if __name__ == "__main__":
     setup()
 
-
-
-# setup(name='mesaPlot',
-#       version='2.0.0',
-#       description='Library for reading and plotting MESA data',
-#       license="GPLv2+",
-#       author='Robert Farmer',
-#       author_email='r.j.farmer@uva.nl',
-#       url='https://github.com/rjfarmer/mesaPlot',
-#       packages=["mesaPlot"],
-#       tests_require=["pytest","unittest2"],
-#       test_suite='tests',
-#       python_requires='>=3.6',
-#       classifiers=[
-# 			"Development Status :: 5 - Production/Stable",
-# 			"Intended Audience :: Science/Research",
-# 			"License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
-# 			"Topic :: Scientific/Engineering :: Astronomy",
-#       ],
-#       extras_require={
-# 		'dev': [
-# 			'unittest2'
-# 			]
-# 		}
-#      )
